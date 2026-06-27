ea1d6bfd6e4fc427733c967f3b78c8e00d59569f
Grace Aderinsola Sodipo <aderinsolagrace2006@gmail.com>
2026-06-26 21:20:24 -0500

    Time: 10 ms (17.77%) | Memory: 21.6 MB (10.34%) - LeetSync

---
+++ 136-single-number/single-number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        dict = {}
+        for num in nums:
+            if num not in dict:
+                dict[num] = 1
+            else:
+                dict[num] +=1
+        for key in dict.keys():
+            if dict[key] == 1:
+                return key
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
