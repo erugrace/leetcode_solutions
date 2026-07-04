612e31227440ffc0cd576f70d8322d878ac3d683
Grace Aderinsola Sodipo <aderinsolagrace2006@gmail.com>
2026-07-03 22:41:58 -0500

    Time: 0 ms (100.00%) | Memory: 19.1 MB (94.37%) - LeetSync

---
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1, list2):
+
+        dummy = ListNode()
+        newHead = dummy
+        while list1 and list2:
+            if list1.val < list2.val or list1.val == list2.val:
+                newHead.next = list1
+                list1 = list1.next
+            else:
+                newHead.next = list2
+                list2 = list2.next
+            newHead = newHead.next
+        if list1:
+            newHead.next = list1
+        if list2:
+            newHead.next = list2
+        return dummy.next
+        
+            
+            
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
