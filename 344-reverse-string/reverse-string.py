bf358c313f868a68046d6cceb320276c3875ce04
Grace Aderinsola Sodipo <aderinsolagrace2006@gmail.com>
2026-07-04 15:11:21 -0500

    Time: 0 ms (100.00%) | Memory: 23.5 MB (43.95%) - LeetSync

--- 344-reverse-string/reverse-string.py
+++ 344-reverse-string/reverse-string.py
@@ -4,14 +4,12 @@ class Solution:
         Do not return anything, modify s in-place instead.
         """
         left = 0
-        right = len(s) - 1
-        while left < right:
-            s[left ], s[right] = s[right], s[left]
+        right = len(s)-1
+        while left<right:
+            s[left], s[right] =s[right], s[left]
             left += 1
-            right -= 1
-        
-        
-        
+            right -=1
+        return s
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
