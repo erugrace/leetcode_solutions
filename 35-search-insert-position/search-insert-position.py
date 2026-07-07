5f8e52a5fa6b9c05f94ad74043b16dd7e17a1505
Grace Aderinsola Sodipo <aderinsolagrace2006@gmail.com>
2026-07-06 21:19:30 -0500

    Time: 0 ms (100.00%) | Memory: 19.8 MB (80.58%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -2,12 +2,9 @@ class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         if target in nums:
             return nums.index(target)
-        else:
-            nums.append(target)
-            nums.sort()
-            return nums.index(target)
-
-        
+        nums.append(target)
+        nums.sort()
+        return nums.index(target)
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
