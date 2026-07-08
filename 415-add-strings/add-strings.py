6f28081a488b37df27dfca0debc35fdc8fa96454
Grace Aderinsola Sodipo <aderinsolagrace2006@gmail.com>
2026-07-07 21:28:55 -0500

    Time: 8 ms (10.10%) | Memory: 19.3 MB (70.02%) - LeetSync

--- 415-add-strings/add-strings.py
+++ 415-add-strings/add-strings.py
@@ -1,18 +1,19 @@
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
-        num1, num2 = num1[::-1], num2[::-1]
-        carry = 0
+        num1,num2 = num1[::-1], num2[::-1]
         res = ""
+        carry = 0
         for i in range(max(len(num1), len(num2))):
-            digitA = int(num1[i]) if i < len(num1) else 0
-            digitB = int(num2[i]) if i < len(num2) else 0
-            total = digitA + digitB + carry
+            digit1 = int(num1[i]) if i < len(num1) else 0
+            digit2 = int(num2[i]) if i < len(num2) else 0
+            total = digit1 + digit2 + carry
             res = str(total % 10) + res
             carry = total // 10
+
+
         if carry:
             res = str(carry) + res
         return res
-
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
