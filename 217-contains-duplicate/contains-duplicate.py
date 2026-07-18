05cbed90d3e135760995f0e821825bbcfe7c6f5d
Grace Aderinsola Sodipo <aderinsolagrace2006@gmail.com>
2026-07-17 23:47:11 -0500

    Time: 9 ms (78.09%) | Memory: 32.1 MB (59.41%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.py
@@ -0,0 +1,14 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+
+        return False
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
