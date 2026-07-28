22da6aecb7451f44445b11eec36abd0f627c4ee4
Grace Aderinsola Sodipo <aderinsolagrace2006@gmail.com>
2026-07-27 21:53:36 -0500

    Time: 4 ms (59.96%) | Memory: 20.4 MB (89.27%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.py
@@ -0,0 +1,14 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        left = 0
+        for r in range(len(nums)):
+            if nums[r] != 0:
+                nums[left], nums[r] = nums[r], nums[left]
+                left += 1
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
