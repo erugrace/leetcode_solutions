83b62033b17e56edf9953558b5acbb75732039fc
Grace Aderinsola Sodipo <aderinsolagrace2006@gmail.com>
2026-08-01 00:04:03 -0500

    Time: 0 ms (100.00%) | Memory: 19.3 MB (32.80%) - LeetSync

--- 3396-valid-word/valid-word.py
+++ 3396-valid-word/valid-word.py
@@ -1,27 +1,19 @@
 class Solution:
     def isValid(self, word: str) -> bool:
         vowels = {"a", "e", "i", "o", "u"}
-
         if len(word) < 3:
             return False
-
         has_vowel = False
         has_consonant = False
-
         for s in word:
             if not s.isalnum():
                 return False
-
-            if s.isalpha():
+            if s.isalpha(): 
                 if s.lower() in vowels:
-                    has_vowel = True
+                   has_vowel = True
                 else:
-                    has_consonant = True
-
+                   has_consonant = True
         return has_vowel and has_consonant
-            
-        
-        
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
