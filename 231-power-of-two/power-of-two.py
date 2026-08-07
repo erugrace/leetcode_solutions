8d29973b31cf49a649c3de78cd7d0fb7e62e1100
Grace Aderinsola Sodipo <aderinsolagrace2006@gmail.com>
2026-08-07 02:59:58 +0000

    Time: 0 ms (100.00%) | Memory: 19.5 MB (18.87%) - LeetSync

---
+++ 231-power-of-two/power-of-two.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n == 0:
+            return False
+        return (n & (n-1) == 0)
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
