0bdd8d4177da0f1a605abd62a9713a6f3ad46cad
Grace Aderinsola Sodipo <aderinsolagrace2006@gmail.com>
2026-08-07 06:10:35 +0000

    Time: 0 ms (100.00%) | Memory: 19.3 MB (68.38%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.py
@@ -0,0 +1,14 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        pascal = []       
+        for i in range(numRows):
+            row = [1] * (i+1)
+            for j in range(1,i):
+                prev_row = pascal[-1]
+                row[j] = prev_row[j] + prev_row[j - 1]
+            pascal.append(row)
+        return pascal
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
