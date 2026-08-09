3de657a07fad9fd1ce6d18e80688d68f2ed31796
Grace Aderinsola Sodipo <aderinsolagrace2006@gmail.com>
2026-08-09 03:19:20 +0000

    Time: 39 ms (90.07%) | Memory: 19.3 MB (29.76%) - LeetSync

--- 190-reverse-bits/reverse-bits.py
+++ 190-reverse-bits/reverse-bits.py
@@ -1,8 +1,10 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        binary = bin(n)[2:].zfill(32)
-        reverse = binary[::-1]
-        return int(reverse,2)
+       binary  = bin(n)[2:].zfill(32)
+       reverse=  binary[::-1]
+       return int(reverse,2)
+
+
 
 
 # Synced seamlessly with LeetHub Pro
