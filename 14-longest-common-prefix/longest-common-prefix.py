bcb54357be58045cf41f36567782269f18d23afd
Grace Aderinsola Sodipo <aderinsolagrace2006@gmail.com>
2026-08-09 00:21:32 +0000

    Time: 3 ms (19.34%) | Memory: 19.4 MB (31.20%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,21 +1,14 @@
 class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
-        dict = {}
-        for i in range(len(strs)):
-            dict[i] = len(strs[i])
-        minlength = min(dict.values())
-        for key,value in dict.items(): 
-            if value == minlength:
-                minindex = key
+    def longestCommonPrefix(self, strs: List[str]) -> str:        
+        res = ""
+        for i in range(len(strs[0])):
+            for j in strs:
+                # Fix: Check length first to avoid IndexError
+                if i == len(j) or j[i]!= strs[0][i] :
+                    return res
+            res = res + strs[0][i]
+        return res
 
-        prefix = ""
-        for i in (range(len(strs[minindex]))):
-            for j in range(1, len(strs)):
-                if strs[j][i] != strs[0][i]:
-                    return prefix
-            prefix= prefix + strs[0][i]
-        return prefix
-        
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
