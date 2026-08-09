0890f9b383a8a0073b8ee88db950ca4a4106c324
Grace Aderinsola Sodipo <aderinsolagrace2006@gmail.com>
2026-08-09 03:37:11 +0000

    Time: 0 ms (100.00%) | Memory: 19.3 MB (72.97%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -3,12 +3,17 @@ class Solution:
         res = ""
         for i in range(len(strs[0])):
             for j in strs:
-                # Fix: Check length first to avoid IndexError
-                if i == len(j) or j[i]!= strs[0][i] :
+                if len(j) == i or j[i] != strs[0][i]:
                     return res
             res = res + strs[0][i]
         return res
+        # [bag, bat , bed]
 
+        # bag
+        #     bat
+        #     bed
+
+        # [ "flower","flow"]
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
