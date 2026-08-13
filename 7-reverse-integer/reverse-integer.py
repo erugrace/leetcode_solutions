1f55f2e31494bd7e61acf7eeaf83309a522d0926
Grace Aderinsola Sodipo <aderinsolagrace2006@gmail.com>
2026-08-13 19:12:11 +0000

    Time: 51 ms (24.91%) | Memory: 19.4 MB (8.60%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -1,22 +1,17 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        if x < 0:
-            newx = -1 * (x)
-        else:
-            newx = x
-        integers = ["1","2","3","4","5","6","7","8","9","10"]
-        newS = ""
-        newx = str(newx)
-        
-        for i in range(len(newx)):
-            newS =newx[i] + newS
-        newS = int(newS)
-        if x < 0:
-            newS = -1 * newS
-        if newS < -2**31 or newS > 2**31 - 1:
-           return 0
-        return newS
-        
+        MAX = 2147483647
+        MIN = -2147483648
+        res = 0 
+        while x:
+            digit = int(math.fmod(x,10))
+            x = int(x/10)
+            if res > MAX//10 or res == MAX//10 and digit >= MAX % 10:
+                return 0
+            if res < MIN//10 or res == MIN//10 and digit <= MIN %10:
+                return 0
+            res = (res*10) + digit
+        return res
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
