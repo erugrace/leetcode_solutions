454d5d4df6084724011f7de019ae686d63643d63
Grace Aderinsola Sodipo <aderinsolagrace2006@gmail.com>
2026-08-13 02:25:43 +0000

    Time: 2 ms (48.83%) | Memory: 19.2 MB (84.70%) - LeetSync

--- 67-add-binary/add-binary.py
+++ 67-add-binary/add-binary.py
@@ -1,16 +1,18 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
         res = ""
-        a,b = a[::-1], b[::-1]
         carry = 0
-        for n in range(max(len(a), len(b))):
-            digit1 = int(a[n]) if n < len(a) else 0
-            digit2 = int(b[n]) if n < len(b) else 0
+        a,b = a[::-1], b[::-1]
+        for i in range(max(len(a),len(b))):
+            digit1 = int(a[i]) if i < len(a) else 0
+            digit2 = int(b[i]) if i <len(b) else 0
             total = digit1 + digit2 + carry
             res = str(total % 2) + res 
-            carry= total // 2
-        if carry: 
+            carry = total // 2
+        if carry:
             res = str(carry) + res
+
+
         return res
 
 # Synced seamlessly with LeetHub Pro
