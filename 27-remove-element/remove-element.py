570883787f5fa26a2adf1de436fa5cf32ec8496c
Grace Aderinsola Sodipo <aderinsolagrace2006@gmail.com>
2026-08-14 22:44:51 +0000

    Time: 0 ms (100.00%) | Memory: 19.3 MB (57.00%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -1,11 +1,10 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         
-        placeholder  = val + 1
         l = 0 
         for r in range(len(nums)):
             if nums[r] != val:
-                nums[l],nums[r] = nums[r],nums[l]
+                nums[l] = nums[r]
                 l += 1
         return l
 
