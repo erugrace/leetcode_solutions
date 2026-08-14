918b5a81c6cc0e23638ec347311a240ab9224131
Grace Aderinsola Sodipo <aderinsolagrace2006@gmail.com>
2026-08-14 21:40:14 +0000

    Time: 0 ms (100.00%) | Memory: 19.2 MB (79.12%) - LeetSync

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.py
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.py
@@ -1,11 +1,12 @@
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
         result = []
-        total = 0
+        total = 0 
         for num in nums:
             total += num
             result.append(total)
         return result
+
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
