7ca175b1a4dc68c23c0fbf456d0659a5d575d5a6
Grace Aderinsola Sodipo <aderinsolagrace2006@gmail.com>
2026-08-14 21:01:11 +0000

    Time: 0 ms (100.00%) | Memory: 19.3 MB (42.00%) - LeetSync

--- 593-valid-square/valid-square.py
+++ 593-valid-square/valid-square.py
@@ -1,19 +1,18 @@
 class Solution:
     def validSquare(self, p1: List[int], p2: List[int], p3: List[int], p4: List[int]) -> bool:
-
         def distance(a,b):
-            return (a[0]-b[0])**2 + (a[1]-b[1])**2 
-        dists = []
-        points = [p1, p2, p3, p4]
+            return (a[0]- b[0])**2 + (a[1]- b[1])**2
+        points = [p1,p2,p3,p4]
+        dist = []
         for i in range(4):
             for j in range(i+1, 4):
-                dists.append(distance(points[i],points[j]))
-        dists.sort()
-        if dists[0] > 0 and dists[0]==dists[1] == dists[2] == dists[3] and dists[4] == dists[5]:
+                dist.append(distance(points[i],points[j]))
+        dist.sort()
+        if dist[0] != 0 and dist[0] == dist[1] == dist[2] == dist[3] and dist[4] == dist[5]:
             return True
         return False
 
-            
+
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
