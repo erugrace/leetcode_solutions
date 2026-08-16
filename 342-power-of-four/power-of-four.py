6ebe44d078f6d7a7c58a745b31a1db4c6fd6f1aa
Grace Aderinsola Sodipo <aderinsolagrace2006@gmail.com>
2026-08-16 03:04:45 +0000

    Time: 0 ms (100.00%) | Memory: 19.4 MB (23.40%) - LeetSync

--- 342-power-of-four/power-of-four.py
+++ 342-power-of-four/power-of-four.py
@@ -1,6 +1,6 @@
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
-        return n > 0 and (n & (n - 1)) == 0 and (n & 0x55555555) != 0
+        return n > 0 and (n & (n-1)) == 0 and (n-1)% 3 == 0
 
        
         
