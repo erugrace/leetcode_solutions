94e251f650a784df81c2089b628d5e90e1100ffc
Grace Aderinsola Sodipo <aderinsolagrace2006@gmail.com>
2026-08-22 03:20:00 +0000

    Time: 7 ms (37.59%) | Memory: 19.4 MB (25.79%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.py
@@ -0,0 +1,27 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        values = [
+            1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4, 1
+        ]
+
+        symbols = [
+            "M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV", "I"
+        ]
+
+        result = ""
+
+        for i in range(len(values)):
+            while num >= values[i]:
+                num -= values[i]
+                result += symbols[i]
+
+        return result
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
