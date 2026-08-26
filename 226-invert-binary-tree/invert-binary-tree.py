e95b0ca8e707d9fb53ef02eef16492e65bf3360d
Grace Aderinsola Sodipo <aderinsolagrace2006@gmail.com>
2026-08-26 15:52:52 -0500

    Time: 0 ms (100.00%) | Memory: 19.3 MB (22.09%) - LeetSync

---
+++ 226-invert-binary-tree/invert-binary-tree.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return
+        root.left, root.right = root.right, root.left
+        self.invertTree(root.right)
+        self.invertTree(root.left)
+        
+        return root
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
