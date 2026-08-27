834b1c10cd8b37b7b2141d0ab3097eb65123399f
Grace Aderinsola Sodipo <aderinsolagrace2006@gmail.com>
2026-08-26 22:11:47 -0500

    Time: 0 ms (100.00%) | Memory: 19.4 MB (65.82%) - LeetSync

---
+++ 404-sum-of-left-leaves/sum-of-left-leaves.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        if root.left and not root.left.left and not root.left.right:
+               return root.left.val + self.sumOfLeftLeaves(root.right)
+        return self.sumOfLeftLeaves(root.left) + self.sumOfLeftLeaves(root.right)
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
