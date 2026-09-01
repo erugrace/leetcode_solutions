79e4a510716e5b7eaea06af1257dd546720b1fb5
Grace Aderinsola Sodipo <aderinsolagrace2006@gmail.com>
2026-09-01 11:23:44 -0500

    Time: 0 ms (100.00%) | Memory: 19.4 MB (33.43%) - LeetSync

---
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        queue = deque([root])
+        result =[]
+        while queue:
+            level_size= len(queue)
+            for i in range(level_size):
+                node = queue.popleft()
+                if i == level_size -1:
+                    result.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        return result
+            
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
